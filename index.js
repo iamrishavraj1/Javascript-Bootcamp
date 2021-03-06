fd76995e201a0a452df6dec115828df9718421dd
Rishav Raj <iamrishavraj1@gmail.com>
2021-03-06 22:17:01 +0530

    JavaScript

--- index.js
+++ index.js
@@ -852,6 +852,7 @@
 //Challange Time.
 
 //1.
-new Date().toLocaleTimeString();
-new Date().toLocaleDateString();
-new Date().toLocaleString();
+// new Date().toLocaleTimeString();
+// new Date().toLocaleDateString();
+// new Date().toLocaleString()
+
