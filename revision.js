61096d2f79df83d35d32110c0e7f948d0d36cf70
Rishav Raj <iamrishavraj1@gmail.com>
2021-03-11 21:25:06 +0530

    JS Revison

--- revision.js
+++ revision.js
@@ -57,3 +57,22 @@
 
 // const prices = [200,300,500,250,10,1000,600];
 // const findElem = console.log(prices.filter((currVal)=>currVal>400));
+//GFG,BIT-D
+
+// let a1 = ["1","2", "3","4","5","6"];
+// let a2 = ["1", "2", "3", "5"];
+
+// console.log(a1.filter(e=>!a2.includes(e))); 
+
+//Solutions
+
+//filter() make a new array with the elements the pass truly by given condition.
+//includes() check the value that are present in array or not.
+//Here, you See
+//a1.filter make a new array where the condition said that to print all the values of a1 is not present in a2.
+
+
+//pop()
+const plants = ["hello","bello","huol"]
+console.log(plants.unshift("Gg"));
+console.log(plants); 
